ecfda83d9645d9d4456f30681e4e7d1556c898fb
Julio Nicolas <jnicolas@praga.ws>
2018-08-06 19:01:14 -0600

    Cambio string en mongo

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var port = process.env.PORT || 3000;
 var app = require('./app');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://test:testEdutec1@ds231961.mlab.com:31961/edutec-nodejs-zoo')
+//mongoose.connect('mongodb://test:testEdutec1@ds231961.mlab.com:31961/edutec-nodejs-zoo')
+mongoose.connect('mongodb://jnicolas:nicoss1@ds231961.mlab.com:31961/edutec_zoo_nico')
 //mongoose.connect('mongodb://localhost:27017/zoo');
 
 app.listen(port);

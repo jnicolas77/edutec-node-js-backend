6b535b74c4e77751ede406c426bba86c7dcec287
jnicolas78 <JulNicsannin@gmail.com>
2018-08-06 18:45:04 -0600

    cambio de string de conexion

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var port = process.env.PORT || 3000;
 var app = require('./app');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://test:testEdutec1@ds231961.mlab.com:31961/edutec-nodejs-zoo')
-mongoose.connect('mongodb://localhost:27017/zoo');
+mongoose.connect('mongodb://test:testEdutec1@ds231961.mlab.com:31961/edutec-nodejs-zoo')
+//mongoose.connect('mongodb://localhost:27017/zoo');
 
 app.listen(port);
 console.log('Edutec Backend is running')
